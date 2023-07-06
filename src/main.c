8502baeaee064cdedea982fccc899874adb24664
MarcosG <marcosgranja317@gmail.com>
2023-07-06 13:50:25 +0200

    Add basic SDL window creation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 #include <stdio.h>
 
+// Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main() {
-    printf("Hello World!\n");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+
+    window = SDL_CreateWindow("dum", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x55));
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e;
+    bool quit = false;
+    while (quit == false) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
