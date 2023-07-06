82e377662b744c914ce94126e5f2d4b0eb93fc9a
MarcosG <marcosgranja317@gmail.com>
2023-07-07 00:19:54 +0200

    User Renderer instead of Surface

--- src/main.c
+++ src/main.c
@@ -7,23 +7,28 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main() {
+    SDL_Log("log CUM");
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        SDL_LogCritical(0, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window *window = NULL;
-    SDL_Surface *screenSurface = NULL;
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
+    // Create window
     window = SDL_CreateWindow("dum", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_LogCritical(0, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x55));
-    SDL_UpdateWindowSurface(window);
+    // Create renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (window == NULL) {
+        SDL_LogCritical(0, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
     SDL_Event e;
     bool quit = false;
